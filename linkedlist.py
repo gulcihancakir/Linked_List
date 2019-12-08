4b2279bc205552e076c891eea7e087c760f7187f
gulcihancakir <gulcihancakir8@gmail.com>
2019-12-08 13:15:47 +0300

    commit

--- linkedlist.py
+++ linkedlist.py
@@ -5,8 +5,26 @@ class Linked_List:
     def __init__(self):
         self.__head = None
         self.__tail = None
+        self.len = 0
 
-    def insertRear(self, Node):
+    def insert_front(self, Node):
+        if self.__head == None:
+            self.__head = Node
+            self.__tail = Node
+
+        else:
+            Node.next = self.__head
+            self.__head = Node
+
+
+    def remove_front(self):
+
+        if self.__head != None:
+            q = self.__head
+            self.__head = q.next
+            q = None
+
+    def insert_rear(self, Node):
 
         if self.__head == None:
             self.__head = Node
@@ -16,17 +34,14 @@ class Linked_List:
             self.__tail.next = Node
             self.__tail = Node
 
-    def insertFront(self, Node):
-        if self.__head == None:
-            self.__head = Node
-            self.__tail = Node
-
-        else:
-            Node.next = self.__head
-            self.__head = Node
-
-            return self.__head, self.__tail
         
+
+    def remove_rear(self):
+        print("tail", self.__tail.data)
+        for i in self.travers_list():
+            if i.next == self.__tail:
+                i.next = None
+                self.__tail = i
 
     def travers_list(self):
 
@@ -34,3 +49,4 @@ class Linked_List:
         while node != None:
             yield node
             node = node.next
+            self.len += 1
--- main.py
+++ main.py
@@ -6,10 +6,16 @@ from node import Node
 linked_list = Linked_List()
 
 newNode = Node(4)
-linked_list.insertFront(newNode)
+linked_list.insert_front(newNode)
 
+linked_list.remove_front()
 newNode = Node(9)
-linked_list.insertRear(newNode)
+linked_list.insert_rear(newNode)
+
+newNode = Node(92)
+linked_list.insert_rear(newNode)
 
 for i in linked_list.travers_list():
     print(i.data)
+
+linked_list.remove_rear()
